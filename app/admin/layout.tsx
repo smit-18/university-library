4bd7be76c1855428be577c92310c49f074b7c1f6
Smit Vekaria <smitvekaria56@gmail.com>
2025-01-14 23:29:25 -0500

    Implement Create Book Action

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,10 @@
 import { auth } from "@/auth";
 import Header from "@/components/admin/Header";
 import Sidebar from "@/components/admin/Sidebar";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import "@/styles/admin.css";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -9,6 +12,15 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 	const session = await auth();
 
 	if (!session?.user?.id) redirect("/sign-in");
+
+	const isAdmin = await db
+		.select({ isAdmin: users.role })
+		.from(users)
+		.where(eq(users.id, session.user.id))
+		.limit(1)
+		.then(res => res[0]?.isAdmin === "ADMIN");
+
+	if (!isAdmin) redirect("/");
 
 	return (
 		<main className="flex min-h-screen w-full flex-row">
